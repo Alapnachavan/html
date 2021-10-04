6df86765dd869c236cd44d38ed4a88b550788ad4
Alapnachavan <90903743+Alapnachavan@users.noreply.github.com>
2021-10-05 01:21:55 +0530

    Add files via upload

---
+++ chatbox.py
@@ -0,0 +1,83 @@
+ Data=input("check whether the person is online or offline (on or off): ")
+if Data=="on" :
+  print("The person is online.")
+chat_1=input("Enter the first message : ")
+if chat_1 === "Hello ! " :
+  print ("Hey !! " )
+  chat_2 = input ("Enter the second message : ")
+  if chat_2 == "How are you? : " :
+     print ("I'm great, thank you. What about you? ") 
+     if chat_3 == " I'm good, thank you. " :
+        print(" Yeah :) " )
+        chat_3 = int(input("enter the number : "))
+        if chat_3 ==input("Enter the fourth message : ")
+            print 
+            if chat_4 == Which grade you're in now ? " :
+
+print("Currently I'm pursuing my grade 12. " ) chat 5=input("Enter the fifth message : ")
+
+if chat 5 == "What is your age ? " :
+
+print ("I'm 18 years old now. ) W chat 6 = input("Enter the sixth message : "): if chat_6 "Oh wow, so finally you're officially an adult ! " :
+
+print("Hehe " )
+
+chat_7 input ( "Enter the seventh message: ") =
+
+if chat_7 I'm nineteen years old now. Not a lot of difference huh ?
+
+print ( yess! " )
+
+chat_8 = input("Enter the eighth message : ")
+
+if chat_8 == " I don't even remember the last time when we met. " print("I know right. We surely need to have a get-togther ASAP. " ) chat_9=input("Enter the nineth message: ")
+
+if chat 9 == "Let's meet! What's say ? " :
+
+print ( "YESSSSS! Why not ! " )
+
+chat_10 input("Enter the tenth message: ")
+[7:31 PM, 9/20/2021] Aaditi Ng: print(" Yeah, yeah true.´)
+
+else:
+
+print (ERROR 1 - )
+
+print("ERROR 1 )
+
+else:
+
+print ("ERROR 1 " )
+
+else:
+
+print ("ERROR 1 )
+
+glse:
+
+print ("ERROR 1 " )
+
+else:
+
+print("ERROR 1
+
+else:
+
+print ( ERROR 1 ")
+
+else:
+
+print ("ERROR 1 " )
+
+else:
+
+print ("ERROR 1 " )
+
+else:
+
+print(" ERROR 1 " )
+
+else:
+
+print ( "The person nis offline. "
+[7:30 PM, 9/20/2021] Aaditi Ng: if chat 10 ==> We need to ask our parents for the permission before meeting physically
